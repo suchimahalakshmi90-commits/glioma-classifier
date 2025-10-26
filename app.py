0f8b4aba916c7be9dba0d45a8ae3d70331ae1b89
mahalakshmi k s <suchimahalakshmi90@gmail.com>
2025-10-26 17:15:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -233,4 +233,7 @@ def result_file(filename):
     return send_from_directory(app.config['RESULT_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
+
+
